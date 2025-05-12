4b86c6c937f145d3ec7fa952763163b5037c93e4
artur <brugeman.artur@gmail.com>
2025-05-12 13:46:42 +0200

    Fix volumes

--- src/modules/key.ts
+++ src/modules/key.ts
@@ -1,9 +1,10 @@
 import fs from "node:fs";
 import { bytesToHex } from "@noble/hashes/utils";
 import { generateSecretKey } from "nostr-tools";
+import { HOME_PATH } from "./consts";
 
 export function getSecretKey() {
-  const FILE = "./.service.sk";
+  const FILE = HOME_PATH+"/.service.sk";
   if (fs.existsSync(FILE)) {
     const hex = fs.readFileSync(FILE).toString("utf8");
     const privkey = Buffer.from(hex, "hex");
